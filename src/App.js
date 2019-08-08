0d859e741c60d90c6bfb53956af3df8e99f0a0c9
pixellab <pixel-lab>
2019-08-08 09:03:17 +0530

    challange

--- src/App.js
+++ src/App.js
@@ -4,21 +4,42 @@ import './App.css';
 
 class App extends React.Component {
   state ={
-    counter: 0
+    counter: 0,
+    error: false
   }
   render(){  
     return (
     <div className="App">
       <div data-test="counter">
-         the counter is {this.state.counter}
+         the counter is {this.state.counter} <br/><br/>
+         {this.state.error ? <span style={{color:'red', fontSize:'25px'}}>Counter Will not lessthan 0</span> : null}
       </div>
       <button  data-test="button" onClick={
         () => {
+          if(this.state.counter === 0){
+            this.setState({
+              error: false
+            })
+          }
           this.setState({
             counter: this.state.counter+1
           })
         }
-      }>Update Counter</button>
+      }>++ Counter</button>
+      <button  data-test="button" onClick={
+        () => {
+          if(this.state.counter){
+            this.setState({
+              counter: this.state.counter - 1
+            })
+          }
+          else {
+            this.setState({
+              error: true
+            })
+          }
+        }
+      }>-- Counter</button>
     </div>
   );
 }
